b53145b378102a26bbb163379ad0358673e954cf
Nicolas <n_skatchkovsky@msn.com>
2020-07-20 09:28:04 +0100

    tests

--- launch_job.sh
+++ launch_job.sh
@@ -6,9 +6,8 @@
 
 python /users/k1804053/snn_private/launch_experiment.py --where=rosalind --dataset=mnist_dvs_10_binary --n_h=512 --n_output_enc=512 --num_samples_train=30000 \
 --test_period=2000 --num_ite=1 --suffix=_snr_m_6 \
---model=jscc --systematic=false --snr=-6 --r=0.3 --labels 1 7
-
-#--save_path=/users/k1804053/results/009__23-06-2020_mnist_dvs_10_binary_wispike_30000_epochs_nh_512_nout_512_snr_m_6/
+--model=wispike --systematic=false --snr=0 --r=0.3 --labels 1 7 \
+--save_path=/users/k1804053/results/025__15-07-2020_mnist_dvs_10_binary_wispike_30000_epochs_nh_338_nout_338_snr_0/
 
 #python /users/k1804053/snn/launch_experiment.py --where=rosalind --dataset=mnist_dvs_10 --n_h=128 --num_samples_train=100000 --test_period=5000 --num_ite=1 --suffix=_teacher_forcing \
 #--model=wta
