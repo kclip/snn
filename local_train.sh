b1a26f2569c8cc9061229d8e6e481b65013888d4
Nicolas <n_skatchkovsky@msn.com>
2020-08-03 15:24:39 +0100

    tests

--- local_train.sh
+++ local_train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 gcloud compute ssh --zone europe-west1-b $1 \
 --command "cd /home/k1804053/snn_private && /opt/conda/bin/python mnist_online_distributed.py --dist_url='tcp://10.132.0.9:23456' \
- --world_size=$2 --node_rank=$3 --processes_per_node=$4 --tau=16 --test_interval=25 --num_samples_train=500 \
+ --world_size=$2 --node_rank=$3 --processes_per_node=$4 --tau=32 --test_interval=25 --num_samples_train=500 \
   --n_hidden_neurons=64 --num_ite=3 --save_path=/home/k1804053/results/results_flsnn/ --labels 1 7"
 
 
