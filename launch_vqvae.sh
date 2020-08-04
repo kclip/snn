98ae3a502bc106b28b35a1e30e037a909d1e0496
Nicolas <n_skatchkovsky@msn.com>
2020-08-04 10:12:43 +0100

    tests

--- launch_vqvae.sh
+++ launch_vqvae.sh
@@ -4,5 +4,6 @@
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --partition=nms_research
 
-python /users/k1804053/snn_private/train_vqvae.py --where=rosalind --num_samples_train=10000 --test_period=1000 --num_ite=1 --labels 1 7 \
---suffix=_snr_0 --snr=0
+python /users/k1804053/snn_private/train_vqvae.py --where=rosalind --num_samples_train=10000 --test_period=100 \
+--num_ite=1 --labels 1 7 --snr=0 \
+--n_frames=80
