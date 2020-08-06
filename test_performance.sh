1634716aeb9fc1f10bad1205717657c1d8f2a1a0
Nicolas <n_skatchkovsky@msn.com>
2020-08-06 17:59:53 +0100

    tests

--- test_performance.sh
+++ test_performance.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python comparison_performance.py --model=vqvae --weights=049__04-08-2020_vqvae_snn_10000_epochs_nh_256_ny_10_nframes_80 --classifier=snn --classifier_weights=002__18-06-2020_mnist_dvs_10_binary_binary_20000_epochs_nh_256_nout_128
+python comparison_performance.py --model=vqvae --weights=049__04-08-2020_vqvae_snn_10000_epochs_nh_256_ny_10_nframes_80 --classifier=mlp --classifier_weights=001__04-08-2020_vqvae_mlp_9000_epochs_nh_256_ny_720_nframes_80
 
 read wait
