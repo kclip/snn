d567dca0360ea1c7784760333368be8ba124f640
Nicolas <n_skatchkovsky@msn.com>
2020-08-10 12:16:50 +0100

    tests

--- test_performance.sh
+++ test_performance.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=nms_research
 
 python comparison_performance.py --where=rosalind \
- --weights=054__06-08-2020_mnist_dvs_10_binary_wispike_50000_epochs_nh_676_nout_676_snr_0 \
+ --weights=025__15-07-2020_mnist_dvs_10_binary_wispike_30000_epochs_nh_338_nout_338_snr_0 \
   --model=wispike --snr_list 0
 
 read wait
