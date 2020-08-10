33a53c711bd8e67c347ec66191b2116eabd9d3a5
Nicolas <n_skatchkovsky@msn.com>
2020-08-10 16:45:47 +0100

    tests

--- launch_gpu_job.sh
+++ launch_gpu_job.sh
@@ -6,6 +6,6 @@
 #SBATCH --constrain=v100
 
 python /users/k1804053/snn_private/launch_experiment.py --where=rosalind --dataset=mnist_dvs_10_binary \
---n_h=1014 --n_output_enc=1014 --num_samples_train=50000 \
---test_period=5000 --num_ite=3 --suffix=_snr_m_6 \
---model=wispike --systematic=false --snr=-6 --r=0.3 --labels 1 7 --disable-cuda=false
+--n_h=338 --n_output_enc=338 --num_samples_train=50000 \
+--test_period=5000 --num_ite=3 --suffix=_snr_0 \
+--model=wispike --systematic=false --snr=0 --r=0.3 --labels 1 7 --disable-cuda=false
