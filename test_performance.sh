cc380d35da7688a8fe02b5d7b7663b411f7dd294
Nicolas <n_skatchkovsky@msn.com>
2020-08-10 12:14:27 +0100

    tests

--- test_performance.sh
+++ test_performance.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#SBATCH --ntasks=16
+#SBATCH --ntasks=8
 #SBATCH --time=168:00:00
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --partition=nms_research
 
 python comparison_performance.py --where=rosalind \
- --weights=053__04-08-2020_mnist_dvs_10_binary_wispike_50000_epochs_nh_676_nout_676_snr_m_6 \
+ --weights=047__04-08-2020_mnist_dvs_10_binary_wispike_50000_epochs_nh_338_nout_338_snr_m_6 \
   --model=wispike --snr_list -6
 
 read wait
