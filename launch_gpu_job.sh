f726b426f8d4be7328372ad7e9fda04bd671a005
Nicolas <n_skatchkovsky@msn.com>
2020-09-03 12:35:27 +0100

    tests

--- launch_gpu_job.sh
+++ launch_gpu_job.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu
 #SBATCH --constrain=v100
 
-python /users/k1804053/snn_private/launch_experiment.py --where=rosalind --dataset=mnist_dvs \
+python /users/k1804053/snn_private/launch_experiment.py --where=rosalind --dataset=dvs_gesture \
 --n_h=1048 --num_samples_train=100000 \
 --dt=20000 --sample_length=1800 \
 --test_period=50000 --num_ite=1 --disable-cuda=false
--- launch_job.sh
+++ launch_job.sh
@@ -4,19 +4,7 @@
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --partition=nms_research
 
-#python /users/k1804053/snn_private/launch_experiment.py --where=rosalind --dataset=mnist_dvs_10_binary --n_h=338 --n_output_enc=338 --num_samples_train=30000 \
-#--test_period=2000 --num_ite=1 --suffix=_snr_m_4 \
-#--model=wispike --systematic=false --snr=0 --r=0.3 --labels 1 7  \
-#--save_path=/users/k1804053/results/025__15-07-2020_mnist_dvs_10_binary_wispike_30000_epochs_nh_338_nout_338_snr_0/
-
-python /users/k1804053/snn_private/launch_experiment.py --where=rosalind --dataset=mnist_dvs_10_binary \
---n_h=64 --num_samples_train=50000 \
---test_period=100 --num_ite=3  \
---r=0.3 --labels 1 7
-
-
-#python /users/k1804053/snn/launch_experiment.py --where=rosalind --dataset=mnist_dvs_10 --n_h=128 --num_samples_train=100000 --test_period=5000 --num_ite=1 --suffix=_teacher_forcing \
-#--model=wta
-#python launch_experiment.py --where=gcloud --dataset=mnist_dvs_10_binary --n_h=128 --n_output_enc=64 --num_samples_train=100000 \
-#--model=wispike --systematic=true --snr=5 --r=0.3 --labels 1 7 \
-#--test_period=5000 --num_ite=1 --suffix=_sytematic_snr_5_2_labels
+python /users/k1804053/snn_private/launch_experiment.py --where=rosalind --dataset=mnist_dvs \
+--n_h=512 --num_samples_train=100000 \
+--dt=25000  \
+--test_period=50000 --num_ite=1
