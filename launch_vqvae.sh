d1734ba269e1739cacec5d47ef0cafe51cee78ea
Nicolas <n_skatchkovsky@msn.com>
2020-09-10 16:13:57 +0100

    cleaning

--- launch_vqvae.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks=16
-#SBATCH --time=168:00:00
-#SBATCH --mem-per-cpu=8GB
-#SBATCH --partition=nms_research
-
-python /users/k1804053/snn_private/train_vqvae.py --where=rosalind --num_samples_train=1000 --test_period=100 \
---num_ite=1 --labels 1 7 --snr=0 \
---n_frames=10
--- test_performance.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#SBATCH --ntasks=12
-#SBATCH --time=168:00:00
-#SBATCH --mem-per-cpu=8GB
-#SBATCH --partition=nms_research
-
-python comparison_performance.py --where=rosalind \
- --weights=067__27-08-2020_vqvae_mlp_1000_epochs_nh_256_ny_720_nframes_5 \
-  --model=vqvae --n_frames=5 --snr_list 0 -6
