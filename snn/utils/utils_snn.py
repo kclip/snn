fbbfc3dd6c81c76ad1c8701aeed22093f7fa134f
Nicolas <n_skatchkovsky@msn.com>
2020-12-02 17:46:38 +0100

    updated target outputs

--- snn/utils/utils_snn.py
+++ snn/utils/utils_snn.py
@@ -27,12 +27,13 @@ def get_acc_and_loss(network, hdf5_group, test_indices, T, classes, input_shape,
     outputs = torch.zeros([len(test_indices), network.n_output_neurons, T])
     loss = 0
 
-    true_classes = torch.LongTensor(hdf5_group.labels[test_indices, 0])
+    true_classes = torch.FloatTensor()
 
     for j, idx in enumerate(test_indices):
         refractory_period(network)
 
         inputs, lbl = get_example(hdf5_group, idx, T, classes, input_shape, dt, x_max, polarity)
+        true_classes = torch.cat((true_classes, lbl))
         inputs = inputs.to(network.device)
 
         for t in range(T):
@@ -41,6 +42,7 @@ def get_acc_and_loss(network, hdf5_group, test_indices, T, classes, input_shape,
             outputs[j, :, t] = network.spiking_history[network.output_neurons, -1].cpu()
 
     predictions = torch.max(torch.sum(outputs, dim=-1), dim=-1).indices
+    true_classes = torch.max(torch.sum(true_classes, dim=-1), dim=-1).indices
     acc = float(torch.sum(predictions == true_classes, dtype=torch.float)) / len(predictions)
 
     return acc, loss
@@ -57,12 +59,13 @@ def get_acc_loss_and_spikes(network, hdf5_group, test_indices, T, classes, input
     outputs = torch.zeros([len(test_indices), network.n_output_neurons, T])
     loss = 0
 
-    true_classes = torch.LongTensor(hdf5_group.labels[test_indices, 0])
+    true_classes = torch.FloatTensor()
 
     for j, idx in enumerate(test_indices):
         refractory_period(network)
 
         inputs, lbl = get_example(hdf5_group, idx, T, classes, input_shape, dt, x_max, polarity)
+        true_classes = torch.cat((true_classes, lbl))
         inputs = inputs.to(network.device)
 
         for t in range(T):
@@ -72,6 +75,7 @@ def get_acc_loss_and_spikes(network, hdf5_group, test_indices, T, classes, input
             spikes[j, :, t] = network.spiking_history[network.learnable_neurons, -1].cpu()
 
     predictions = torch.max(torch.sum(outputs, dim=-1), dim=-1).indices
+    true_classes = torch.max(torch.sum(true_classes, dim=-1), dim=-1).indices
     acc = float(torch.sum(predictions == true_classes, dtype=torch.float)) / len(predictions)
 
     return acc, loss, spikes
--- snn/utils/utils_wtasnn.py
+++ snn/utils/utils_wtasnn.py
@@ -20,12 +20,13 @@ def get_acc_and_loss(network, hdf5_group, test_indices, T, classes, input_shape,
     outputs = torch.zeros([len(test_indices), network.n_output_neurons, network.alphabet_size, T])
 
     loss = 0
-    true_classes = hdf5_group.labels[test_indices, 0]
+    true_classes = torch.FloatTensor()
 
     for j, idx in enumerate(test_indices):
         refractory_period(network)
 
         inputs, lbl = get_example(hdf5_group, idx, T, classes, input_shape, dt, x_max, polarity)
+        true_classes = torch.cat((true_classes, lbl))
         inputs = inputs.to(network.device)
 
         for t in range(T):
@@ -34,6 +35,7 @@ def get_acc_and_loss(network, hdf5_group, test_indices, T, classes, input_shape,
             outputs[j, :, :, t] = network.spiking_history[network.output_neurons, :, -1].cpu()
 
     predictions = torch.max(torch.sum(outputs, dim=(-1, -2)), dim=-1).indices
+    true_classes = torch.max(torch.sum(true_classes, dim=(-1, -2)), dim=-1).indices
     acc = float(torch.sum(predictions == true_classes, dtype=torch.float) / len(predictions))
 
     return acc, loss
