fixed baseline computation in snnsgd

snn/models/base.py
@@ -4,6 +4,7 @@ import torch
 from torch.nn.init import _calculate_fan_in_and_fan_out, _no_grad_uniform_
 from snn.utils import filters
 import math
+import numpy as np
 
 class SNNetwork(torch.nn.Module):
     def __init__(self, n_input_neurons, n_hidden_neurons, n_output_neurons, topology, synaptic_filter=filters.base_filter,
@@ -219,6 +220,7 @@ class SNNLayer(torch.nn.Module):
     def forward(self, input_history, target=None, no_update=False):
         ff_trace = self.compute_ff_trace(input_history)
         fb_trace = self.compute_fb_trace()
+
         self.potential = self.compute_ff_potential(ff_trace) + self.compute_fb_potential(fb_trace) + self.bias
 
         outputs = self.generate_spikes(input_history)
@@ -247,14 +249,13 @@ class SNNLayer(torch.nn.Module):
         try:
             outputs = torch.bernoulli(torch.sigmoid(self.potential)).to(self.device)
         except RuntimeError:
-            print('Spiking history')
-            print(self.spiking_history[:, -5:])
             print('Potential')
             print(self.potential)
             print('ff_weights', self.ff_weights.isnan().any())
             print('fb_weights', self.fb_weights.isnan().any())
             print('bias', self.bias.isnan().any())
             print('input_hist', input_history.isnan().any())
+            print('hist', self.spiking_history.isnan().any())
 
 
         return outputs

snn/optimizer/snnsgd.py
@@ -2,6 +2,8 @@ from torch.optim.optimizer import Optimizer, required
 import torch
 from typing import List, Optional
 from torch import Tensor
+import numpy as np
+from copy import deepcopy
 
 
 def snnsgd(params: List[Tensor],
@@ -40,9 +42,9 @@ def snnsgd(params: List[Tensor],
 
                 baseline_den = baseline_den_list[i]
                 if baseline_den is None:
-                    baseline_den = d_p.pow(2).mul(ls)
+                    baseline_den = d_p.pow(2)
                 else:
-                    baseline_den.mul_(dampening).add_(d_p.pow(2).mul(ls), alpha=1 - dampening)
+                    baseline_den.mul_(dampening).add_(d_p.pow(2), alpha=1 - dampening)
                 baseline_den_list[i] = baseline_den
 
                 baseline = baseline_num_list[i] / (1e-7 + baseline_den_list[i])
@@ -57,6 +59,8 @@ def snnsgd(params: List[Tensor],
             d_p = d_p.mul(ls - baseline)
 
         param.add_(d_p, alpha=-lr)
+
+
 
 
 class SNNSGD(Optimizer):

snn/test_layered.py
@@ -44,7 +44,7 @@ network2 = BinarySNN(**make_network_parameters(network_type='snn',
                                                topology=topology
                                                ))
 
-lr = 0.0001
+lr = 0.001
 n_samples = 3
 
 optimizer = SNNSGD([{'params': network.out_layer.parameters(), 'ls': False, 'baseline': False},
@@ -99,25 +99,27 @@ for ite in range(500):
     targets = targets[0].to(network.device)
 
     for t in range(T):
-        for _ in range(n_samples):
-            net_probas, net_outputs, probas_hidden, outputs_hidden = network(inputs[:t].T, targets[:, t], n_samples=n_samples)
+        ### LayeredSNN
+        net_probas, net_outputs, probas_hidden, outputs_hidden = network(inputs[:t].T, targets[:, t], n_samples=n_samples)
 
-            # Generate gradients and KL regularization for hidden neurons
-            out_loss = loss_fn(probas_hidden, outputs_hidden.detach())
-            if probas_hidden is not None:
-                hidden_loss = loss_fn(probas_hidden, outputs_hidden.detach())
-                with torch.no_grad():
-                    kl_reg = - gamma * torch.mean(outputs_hidden * torch.log(1e-7 + probas_hidden / r)
-                                                  + (1 - outputs_hidden) * torch.log(1e-7 + (1 - probas_hidden) / (1 - r)))
-            else:
-                hidden_loss = 0
-                kl_reg = 0
+        # Generate gradients and KL regularization for hidden neurons
+        out_loss = loss_fn(probas_hidden, outputs_hidden.detach())
+        if probas_hidden is not None:
+            hidden_loss = loss_fn(probas_hidden, outputs_hidden.detach())
+            with torch.no_grad():
+                kl_reg = - gamma * torch.mean(outputs_hidden * torch.log(1e-7 + probas_hidden / r)
+                                              + (1 - outputs_hidden) * torch.log(1e-7 + (1 - probas_hidden) / (1 - r)))
+        else:
+            hidden_loss = 0
+            kl_reg = 0
 
         loss = out_loss + hidden_loss
         loss.backward()
+
         optimizer.step(out_loss.detach() + kl_reg)
         optimizer.zero_grad()
 
+        ### Binary SNN
         log_proba = network2(inputs[t],  targets[:, t])
         # Accumulate learning signal
         proba_hidden = torch.sigmoid(network2.potential[network2.hidden_neurons - network2.n_input_neurons])